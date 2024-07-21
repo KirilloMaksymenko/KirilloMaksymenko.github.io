3b6b03df1f2f2fbcb34989c48c3fe0f84773b763
кирило максименко <programistmax0116@gmail.com>
2024-07-21 03:32:34 +0300

    test

---
+++ pages/shop/shop_script.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var div = document.getElementById("filt");
+    var width = document.getElementById("bs").offsetWidth;
+    div.textContent = width + "px";
+});
