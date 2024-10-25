d3e389617ffee7778eb4b21975710dff9eb25f15
KirilloMaksymenko <108404922+KirilloMaksymenko@users.noreply.github.com>
2024-10-25 17:35:38 +0200

    Delete pages directory

--- pages/shop/shop_script.js
+++
@@ -1,5 +0,0 @@
-// document.addEventListener("DOMContentLoaded", function() {
-//     var div = document.getElementById("filt");
-//     var width = document.getElementById("bs").offsetWidth;
-//     div.textContent = width + "px";
-// });
